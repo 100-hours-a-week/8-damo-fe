d6ec3743642eec8e512c46e34be5682d51a0602f
soykeepgoing <soykeepgoing@gmail.com>
2026-02-23 21:59:37 +0900

    ✨ feature: 소켓 unsubscribe에 헤더 추가

--- src/lib/websocket/type.ts
+++ src/lib/websocket/type.ts
@@ -1,12 +1,13 @@
   import type { IMessage, StompSubscription } from "@stomp/stompjs";
 
-  export type SubscriptionKey = "chat-list" | "chat-room";
+  export type SubscriptionKey = "chat-list" | `chat-room-${string}`;
 
   export type MessageHandler = (message: IMessage) => void;
 
   export interface SubscriptionDef {
     destination: string;
     handler: MessageHandler;
+    id: string;
     sub?: StompSubscription;
     generation: number;
   }
