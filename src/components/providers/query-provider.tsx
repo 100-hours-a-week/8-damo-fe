fa7354eabca9094ef775fa4acdcfc3e8f5fe805a
soykeepgoing <soykeepgoing@gmail.com>
2026-03-02 21:31:23 +0900

    🔨 chore: 리액트 쿼리 dev tools 기능 추가

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ReactNode } from 'react'
 
 function createQueryClient() {
@@ -46,6 +47,7 @@ export function QueryProvider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   )
 }
---
+++ src/lib/query-client.ts
@@ -0,0 +1,9 @@
+import { QueryClient } from "@tanstack/react-query";
+import { cache } from "react";
+
+export const getQueryClient = cache(
+  () =>
+    new QueryClient({
+      defaultOptions: { queries: { retry: 1, refetchOnReconnect: true } },
+    })
+);
